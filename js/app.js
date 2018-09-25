7757730614c31c7dd4cddec61bfc5f7da8adc615
John Czekalski <john.czekalski@gmail.com>
2018-09-25 21:48:46 +0200

    added code which executes on the click of the login button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+// gets a new object (the architecture allows us to not have to use the 'new' keyword here)
 var g = G$('John', 'Doe');
 
+// use our chainable methods
 g.log().greet().setLang('es').greet(true);
+
+// using our object on the click of the login button
+$('#login').click(function() {
+
+  // create a new 'Greetr' object (let's pretend we know the name from the login)
+  var loginGrtr = G$('Jane', 'Doe');
+
+  // hide the login on the screen
+  $('#logindiv').hide();
+
+  // fire off an HTML greeting, passing '#greeting' as the selector and the chosen language, and log the welcome as well
+  loginGrtr.setLang($('#lang').val()).HTMLgreeting('#greeting', true).log();
+
+});
